3a06c1a0f76bfe46684ef04e3ddb4c96fd900b2e
Raamprasaadh <ramprasadhsurya@gmail.com>
2021-01-30 18:34:18 +0530

    client : add records working successfully

--- client/src/components/addRecordsComponent.js
+++ client/src/components/addRecordsComponent.js
@@ -1,37 +1,109 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import config from '../config/AddRecordConfig.json';
+import axios from 'axios';
+
 
 export default function AddRecordComponent() {
-    const [input, setInput] = useState({});
+    const [state, setState] = useState(config[1].state);
     
-    const handleChange =(e)=>{
-setInput({
-    ...input,
-    [e.currentTarget.name]: e.currentTarget.value
-})
+    const submit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:3001/accounts/add',state)
+        .then((res)=>window.alert(res))
+        .catch(error=> {console.log(state);
+            window.alert(error)});
+    }
+    
+    const toggleChange =(e,field,aspect)=>{
+       var value = state[field][aspect];
+       setState({
+           ...state,
+           [field]:{...state[field],[aspect]:!value}
+       })
+    }
+    //handlesChange in input type = text, radio, slelect
+    const handleChange = (e, data) => {
+        console.log("handle change of :"+data+" : "+e.target.value);
+        setState({
+            ...state,
+            [data]: e.currentTarget.value
+        })
     }
     return (
         <div className="addRcrd">
-        <div className="pageTitle">
-        Add record            
-        </div>
-        <form onSubmit ={null}>
-        <label>Date</label>
-        {/*<Reactdatepicker />*/}
-        <label>Type : </label>
-        <input onChange = {handleChange} type="text" />
-        <label>Expense/Income/Record : </label>
-        <input onChange = {handleChange} type="text" />
-        <label>Varapu/Partition : </label>
-        <input  onChange = {handleChange} type="text" />
-        <label>Tree No :</label>
-        <input onChange = {handleChange} type="number" />
-        <label>Amount : </label>
-        <input  onChange = {handleChange} type="number" />
-        <label>Type Of Attack : </label>
-        <input onChange = {handleChange} type="text"/>
-        <label >Description</label>
-        <input onChange = {handleChange} type="text"/>
-        </form>
+            <form onSubmit={submit}>
+                {config[0].map((field) => {
+
+                    if (field.type === "text") {
+                        return (
+                            <div key={field.uid}>
+                                <label className="formLabel">{field.label}</label>
+                                <input type={field.type} className="formInput" onChange={(e) => handleChange(e, field.className)} placeholder={field.value} />
+                            </div>)
+                    }
+                    else if (field.type === "radio") {
+                        return (
+                            <div key={field.uid} >
+                                <label className="formLabel">{field.label}</label>
+                                {field.options.map((option) => {
+
+                                    return (
+                                        <label><input type={field.type} value={option} checked={state[field.className] === option} onChange={(e) => handleChange(e, field.className)} />{option}</label>
+                                    )
+                                })} 
+                            </div>
+                        )
+                    }
+                    else if (field.type === "checkbox") {
+                        return (
+                            <div key={field.uid}>
+                                <label className="formLabel">{field.label}</label>
+                                {
+                                    field.options.map((option) => {
+                                        
+                                        return (
+                                            <label><input type={field.type} value={option} onChange={(e) => toggleChange(e, field.className, option)} />{option}</label>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    }
+                    else if (field.type === "number") {
+                        return (
+                            <div key={field.uid}>
+                                <label className="formLabel">{field.label}</label>
+                                <input type={field.type}  onChange={(e) => handleChange(e, field.className)} />        
+                            </div>
+                        )
+
+                    }
+                    else if (field.type === "select") {
+                        return (
+                            <div key={field.uid}>
+                                <label className="formLabel">{field.label}</label>
+                                <select onChange={(e) => handleChange(e, field.className)}>
+                                    {
+                                        <option value="" >select</option>
+                                    }
+                                     
+                                        {
+                                            field.options.map((option)=>{
+                                            return(
+                                                <option value={option} onChange={(e)=>handleChange(e,field.className)}>{option}</option>
+                                            )
+                                         })
+                                        }
+                                    
+                                </select>        
+                            </div>
+                        )
+
+                    }
+
+                })}
+                <button type="submit">Add Record</button>
+            </form>
 
         </div>
     )
